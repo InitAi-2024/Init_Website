0616b37b054ceef39dbddbb58a8427fea5756610
Abhinav <abhinavnair7404@gmail.com>
2024-12-27 18:52:28 +0530

    Refactor CORS configuration and improve code formatting in backend

--- Init.Ai-Website-Backend/index.js
+++ Init.Ai-Website-Backend/index.js
@@ -1,28 +1,32 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require('mongoose');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const cors = require("cors");
 const achievementRouter = require("./routes/Achievement");
 const blogRouter = require("./routes/Blog");
-const PORT = process.env.PORT
-const DB = process.env.MONGO_URI
+const PORT = process.env.PORT;
+const DB = process.env.MONGO_URI;
 
-const app = express()
+const app = express();
 
-app.use(cors({
-    origin: 'https://init-website-dusky.vercel.app/',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}))
-app.use(express.json())
-app.use('/api',achievementRouter)
-app.use('/api',blogRouter)
+const corsOptions = {
+  origin: "https://init-website-dusky.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
-mongoose.connect(DB).then(() => {
-    console.log('connected to mongodb')
-}).catch(err => console.log(err));
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use("/api", achievementRouter);
+app.use("/api", blogRouter);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => console.log(err));
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`live on http://localhost:${PORT}/api/`);
-})
-
+  console.log(`live on http://localhost:${PORT}/api/`);
+});
