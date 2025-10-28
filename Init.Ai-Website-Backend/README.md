a723fefffeb162e3a284d35afb417689cdfbc277
nandininema07 <nandini.nema027@gmail.com>
2025-10-28 14:55:38 +0530

    Cleaned git repo for new website

--- Init.Ai-Website-Backend/README.md
+++
@@ -1,1 +0,0 @@
-# InitAI-Backend
--- Init.Ai-Website-Backend/services/ExpressApp.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const cors = require('cors')
-
-module.exports.App = async (app) => {
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(cors())
-    return app;
-}
